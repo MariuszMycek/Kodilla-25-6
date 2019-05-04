bf664242a2ccdecde99e75d67c6a2a816bc9c3d5
Mariusz Mycek <m.mycek83@gmail.com>
2019-05-04 21:25:04 +0200

    Finish fake google login page

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('css'));
+app.use(express.static('assets'));
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.get('/auth/google', function(req, res) {
+  res.render('google');
+});
+
+app.get('/log_info', function(req, res) {
+  res.render('log_info');
+});
+
+var server = app.listen(3000, 'localhost', function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log(
+    'Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port
+  );
+});
+
+app.use(function(req, res, next) {
+  res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!');
+});
